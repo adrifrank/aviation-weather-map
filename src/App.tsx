cd6936616f459b456447104f1a18500261d93cc2
Adriana Frankiv <adrianafrankiv44@gmail.com>
2025-09-12 12:48:03 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Typography, Container } from '@mui/material';
+
+function App() {
+  return (
+    <Container maxWidth="lg" sx={{ mt: 4 }}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Aviation Weather Map
+      </Typography>
+    </Container>
+  );
+}
+
+export default App;
